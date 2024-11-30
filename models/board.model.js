4687e1461c44e9371359e31f49d8acc6a94ce7e4
SingleThreaded400 <av0232016@gmail.com>
2024-11-30 17:35:21 +0530

    Create board.model.js

---
+++ models/board.model.js
@@ -0,0 +1,58 @@
+/*const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BoardSchema = new Schema({
+  coverImg: {
+    url: String, // Store the binary image data
+    contentType: String // Store the content type of the image
+  },
+  boardTitle:{
+    type: String,
+    required: true
+  },
+  user:{
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  posts:[
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+    }
+  ],
+  createdAt:{
+    type: Date,
+    default: Date.now
+  },
+  
+});
+
+module.exports = mongoose.model('Board', BoardSchema);
+*/
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const BoardSchema = new Schema({
+  coverImg: {
+    url: String,
+    contentType: String
+  },
+  boardTitle: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post',
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+module.exports = mongoose.model('Board', BoardSchema);
